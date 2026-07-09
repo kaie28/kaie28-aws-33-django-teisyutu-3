fe533dfca49e7db9f10f25a8cc0ed89f858510f7
kaie28 <bigor28baakapricho@gmail.com>
2026-07-09 17:09:08 +0900

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -1,7 +1,7 @@
 
-import os　#　1★
-import environ  # 1★django-environライブラリを使うので。
-from pathlib import Path   #　1★
+import os　                                                     # 1★
+import environ                                                  # 1★django-environライブラリを使うので。
+from pathlib import Path                                        # 1★
 
 from pathlib import Path
 from dotenv import load_dotenv
@@ -10,28 +10,28 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-##　1【一連の流れ】本番デプロイでのIPなど変数化の仕組み
+##★1【一連の流れ】本番デプロイでのIPなど変数化の仕組み
 
-# 1-1 【実施初期での指示】★settings.pyファイルが、「一時的にすり替わった本物の機密情報」の.envファイル内容を探して読み込む設定 （★★このBASE_DIRの下に、下記2行を追記）※コードの配置・順序が大事
+# 1-1 【実施初期での指示】                                         #1-1 settings.pyファイルが、「一時的にすり替わった本物の機密情報」の.envファイル内容を探して読み込む設定 （★★このBASE_DIRの下に、下記2行を追記）※コードの配置・順序が大事
 env = environ.Env() 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env')) 
 
 
-# 1-2 【具体的実施の指示】★settings.pyファイルが、「一時的な本物の情報」が入ってる.envファイル(シークレットキーやIP）をコピー。
+# 1-2 【具体的実施の指示】　　　　　　　　　　　　　　　　　　　　　　　#1-2 settings.pyファイルが、「一時的な本物の情報」が入ってる.envファイル(シークレットキーやIP）をコピー。
 SECRET_KEY = env('SECRET_KEY', default= '')　　
 MY_IP = env('MY_IP', default='127.0.0.1') 　　　　
 
 
-# 1-3 【DEBUGでの指示】★唯一このDEBUGだけはenvファイルから「★直接本物の情報」コピーして組み込む指示。
+# 1-3 【DEBUGでの指示】　　　　　　　　　　　　　　　　　　　　　　　　 #1-3 唯一このDEBUGだけはenvファイルから「★直接本物の情報」コピーして組み込む指示。
 DEBUG = env.bool('DEBUG', default=False)　　　
 
 
 # 1-4 【コピーした「本物の機密情報」を保管する箱】
 
-ALLOWED_HOSTS = [
-    MY_IP,               # コピーした本物IPなどを保管する箱」
-    'localhost',         # ←内部(Django)の専用の名前(★住所名を示す)　　　
-    '127.0.0.1',　　　　　# ←内部(Django)の★住所番号(世界共通の127.0.0.1)　
+ALLOWED_HOSTS = [　　　　　　　　　　　　　　　　　　　　　　　　　　　# 1-4
+    MY_IP,               　　　　　　　　　　　　　　　　　　　　　　　# コピーした本物IPなどを保管する箱」
+    'localhost',         　　　　　　　　　　　　　　　　　　　　　　　# ←内部(Django)の専用の名前(★住所名を示す)　　　
+    '127.0.0.1',　　　　　　　　　　　　　　　　　　　　　　　　　　　　# ←内部(Django)の★住所番号(世界共通の127.0.0.1)　
 
 ]
 
@@ -78,17 +78,17 @@ TEMPLATES = [
 
 
 
-# 2  Django玄関窓口から、WSGIで仲介（翻訳）させるための設定。
+#★ 2  WSGIで仲介（翻訳）させるための設定。　　　　　　　　　　　　　　　#2,Django玄関窓口から、Djangon内への仲介
 
 WSGI_APPLICATION = 'todoproject.wsgi:application'
 
 
-# 3 Docker(コンテナ)が再構築・破壊されても、ToDoアプリの文字データ等が消えないための★安全保管庫の住所を設定。
+#★ 3 文字データ等の安全保管庫の住所を設定。　　　　　　　　　　　　　　　#3、Docker(コンテナ)が再構築・破壊されても、ToDoアプリの文字データ等が消えないため
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',　　　　　　
-        'NAME': BASE_DIR / 'db_data' / 'db.sqlite3',    #　←★Dockerの標準機能 「ボリュームマウント（Volume Mount）を使用する(文字が消えない設定)
+        'NAME': BASE_DIR / 'db_data' / 'db.sqlite3',    　　　　　　　#Dockerの標準機能 「ボリュームマウント（Volume Mount）を使用する(文字が消えない設定)
 　　　　　
  
     }
